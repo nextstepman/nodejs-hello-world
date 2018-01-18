89ef76e36d961f21a2e5fc278c61b7ddfa2abac1
Richard Scholz <richard.scholz@senacor.com>
2018-01-18 11:44:33 +0000

    added url

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/authorization-code/callback', function (req, res) {
+  res.send('Hello World!');
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
